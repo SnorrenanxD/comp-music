dc529b3bfc75ebc1c3fb68e1697c8498092a8804
SnorrenanXD <renanvandijk@hotmail.com>
2023-02-19 20:45:40 +0100

    script

---
+++ plot.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(spotifyr)
+
+ind_metal <- get_playlist_audio_features("37i9dQZF1DX29LQDcJ6Xy7")
+
+ggplot(ind_metal, aes(track.popularity, danceability, size = energy, color = track.album.release_date)) + 
+  geom_point(alpha = 0.6) + 
+  xlab(Popularity) +
+  ylab(Danceability) +
+  ggtitle("Popularity of Industrial Metal tracks") +
+  theme(
+    legend.position = "none",
+    plot.title = element_text(size = 16, face = "bold")
+    )
+  
+
