41a0972978f884c1eb7440f5572ae73b5dda2586
SnorrenanXD <renanvandijk@hotmail.com>
2023-02-19 20:47:38 +0100

    polot

--- plot.R
+++ plot.R
@@ -3,7 +3,7 @@ library(spotifyr)
 
 ind_metal <- get_playlist_audio_features("37i9dQZF1DX29LQDcJ6Xy7")
 
-ggplot(ind_metal, aes(track.popularity, danceability, size = energy, color = track.album.release_date)) + 
+ggplot(ind_metal, aes(track.popularity, danceability, size = energy)) + 
   geom_point(alpha = 0.6) + 
   xlab(Popularity) +
   ylab(Danceability) +
